3df615c1ae2f81e6dbb37c0f970b53247187b1fe
Alwifachri <alwifachri30@gmail.com>
2022-12-13 02:37:09 +0700

    add navbar footer to login page

--- app/Views/login.php
+++ app/Views/login.php
@@ -7,34 +7,60 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-
     <title>Login</title>
 </head>
 
 <body>
-    <div class="container">
-        <div class="row justify-content-md-center">
+    <nav class="navbar navbar-expand-lg" style="background-color: #C58940;">
+        <div class="container-fluid">
+            <a class="navbar-brand text-white" href="#">Toast Bread</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active text-white" href="#">Shop</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="#">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-            <div class="col-6">
-                <h1>Sign In</h1>
+    <div class="container" style="margin-top: 25px; margin-bottom: 25px;">
+        <div class="row justify-content-md-center">
+            <div class="col-5" style="background-color: #FAEAB1; height: 450px;">
+                <h1 style="text-align:center; margin: 20px;">Sign In</h1>
                 <?php if (session()->getFlashdata('msg')) : ?>
                     <div class="alert alert-danger"><?= session()->getFlashdata('msg') ?></div>
                 <?php endif; ?>
                 <form action="/login/auth" method="post">
-                    <div class="mb-3">
+                    <div class="mb-3" style="padding: 5px; font-size: 24px;">
                         <label for="InputForEmail" class="form-label">Email address</label>
                         <input type="email" name="email" class="form-control" id="InputForEmail" value="<?= set_value('email') ?>">
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3" style="padding: 5px; font-size: 24px;">
                         <label for="InputForPassword" class="form-label">Password</label>
                         <input type="password" name="password" class="form-control" id="InputForPassword">
                     </div>
-                    <button type="submit" class="btn btn-primary">Login</button>
+                    <div class="text-center" style="margin-top: 40px;">
+                        <button type="submit" class="btn btn-lg" style="background-color: #C58940; color: white;">Login</button>
+                    </div>
                 </form>
             </div>
-
         </div>
     </div>
+    <footer class="text-center text-white fixed-bottom" style="background-color: #C58940;">
+        <!-- Grid container -->
+        <div class="container p-4">
+            <h5>Toast Bread</h5>
+            <h4>Wholesome Bread for all</h4>
+        </div>
+        <!-- Grid container -->
+    </footer>
 
     <!-- Popper.js first, then Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
